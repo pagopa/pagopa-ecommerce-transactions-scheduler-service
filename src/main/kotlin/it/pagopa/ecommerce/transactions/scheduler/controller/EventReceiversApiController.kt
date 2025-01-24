2f858f08f793b86b2fcf59695680c75ee4934fe4
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2025-01-24 11:03:33 +0100

    feat(zero downtime deploy): receiver status service + status poller (#161)
    * feat(zero downtime deploy): receiver status service + status poller
    
    * feat(zero downtime deploy): event receivers api commands (#163)
    
    * feat(zero downtime deploy): event receivers api comnmand
    
    * chore(typo): change redis template parameters
    
    * fix: deployment pipeline (#165)
    
    * fix: enable scaled object for all envs

--- src/main/kotlin/it/pagopa/ecommerce/transactions/scheduler/controller/EventReceiversApiController.kt
+++ src/main/kotlin/it/pagopa/ecommerce/transactions/scheduler/controller/EventReceiversApiController.kt
@@ -1,27 +1,35 @@
 package it.pagopa.ecommerce.transactions.scheduler.controller
 
+import it.pagopa.ecommerce.transactions.scheduler.services.EventReceiverService
 import it.pagopa.generated.scheduler.server.api.EventReceiversApi
 import it.pagopa.generated.scheduler.server.model.DeploymentVersionDto
 import it.pagopa.generated.scheduler.server.model.EventReceiverCommandRequestDto
 import it.pagopa.generated.scheduler.server.model.EventReceiverStatusResponseDto
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
 /** Event receivers commands api controller implementation */
 @RestController
-class EventReceiversApiController() : EventReceiversApi {
+class EventReceiversApiController(
+    @Autowired private val eventReceiverService: EventReceiverService,
+) : EventReceiversApi {
 
     /** Handle new receiver command */
     override suspend fun newReceiverCommand(
         eventReceiverCommandRequestDto: EventReceiverCommandRequestDto
     ): ResponseEntity<Unit> {
-        TODO()
+        return eventReceiverService.handleCommand(eventReceiverCommandRequestDto).let {
+            ResponseEntity.accepted().build()
+        }
     }
 
     /** Returns receiver statuses */
     override suspend fun retrieveReceiverStatus(
         version: DeploymentVersionDto?
     ): ResponseEntity<EventReceiverStatusResponseDto> {
-        TODO()
+        return eventReceiverService.getReceiversStatus(deploymentVersionDto = version).let {
+            ResponseEntity.ok(it)
+        }
     }
 }
