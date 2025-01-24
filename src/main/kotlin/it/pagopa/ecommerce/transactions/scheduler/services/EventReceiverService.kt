250ffdca08295f541fc73c4720f1e100f07c4f93
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2025-01-24 11:04:54 +0100

    feat(zero-downtime-deploy): add control bus integration (#158)
    * feat: add control bus integration
    
    * feat: add junit tests
    
    * feat(zero downtime deploy): add redis stream consumer (#160)
    
    * feat(zero downtime deploy): add redis stream consumer
    
    * feat(zero downtime deploy): receiver status service + status poller (#161)
    
    * feat(zero downtime deploy): receiver status service + status poller
    
    * feat(zero downtime deploy): event receivers api commands (#163)
    
    * feat(zero downtime deploy): event receivers api comnmand
    
    * chore(typo): change redis template parameters
    
    * fix: deployment pipeline (#165)
    
    * fix: enable scaled object for all envs

---
+++ src/main/kotlin/it/pagopa/ecommerce/transactions/scheduler/services/EventReceiverService.kt
@@ -0,0 +1,83 @@
+package it.pagopa.ecommerce.transactions.scheduler.services
+
+import it.pagopa.ecommerce.transactions.scheduler.configurations.RedisStreamEventControllerConfigs
+import it.pagopa.ecommerce.transactions.scheduler.configurations.redis.EventDispatcherCommandsTemplateWrapper
+import it.pagopa.ecommerce.transactions.scheduler.configurations.redis.EventDispatcherReceiverStatusTemplateWrapper
+import it.pagopa.ecommerce.transactions.scheduler.exceptions.NoEventReceiverStatusFound
+import it.pagopa.ecommerce.transactions.scheduler.streams.commands.EventDispatcherReceiverCommand
+import it.pagopa.generated.scheduler.server.model.*
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+/** This class handles all InboundChannelsAdapters events receivers */
+@Service
+class EventReceiverService(
+    @Autowired
+    private val eventDispatcherCommandsTemplateWrapper: EventDispatcherCommandsTemplateWrapper,
+    @Autowired
+    private val eventDispatcherReceiverStatusTemplateWrapper:
+        EventDispatcherReceiverStatusTemplateWrapper,
+    @Autowired private val redisStreamConf: RedisStreamEventControllerConfigs
+) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    suspend fun handleCommand(eventReceiverCommandRequestDto: EventReceiverCommandRequestDto) {
+        val commandToSend =
+            when (eventReceiverCommandRequestDto.command) {
+                EventReceiverCommandRequestDto.Command.START ->
+                    EventDispatcherReceiverCommand.ReceiverCommand.START
+                EventReceiverCommandRequestDto.Command.STOP ->
+                    EventDispatcherReceiverCommand.ReceiverCommand.STOP
+            }
+        logger.info("Received event receiver command request, command: {}", commandToSend)
+        // trim all events before adding new event to be processed
+        val recordId =
+            eventDispatcherCommandsTemplateWrapper.writeEventToStreamTrimmingEvents(
+                redisStreamConf.streamKey,
+                EventDispatcherReceiverCommand(
+                    receiverCommand = commandToSend,
+                    version = eventReceiverCommandRequestDto.deploymentVersion
+                ),
+                0
+            )
+
+        logger.info("Sent new event to Redis stream with id: [{}]", recordId)
+    }
+
+    suspend fun getReceiversStatus(
+        deploymentVersionDto: DeploymentVersionDto?
+    ): EventReceiverStatusResponseDto {
+        val lastStatuses =
+            eventDispatcherReceiverStatusTemplateWrapper.allValuesInKeySpace?.filter {
+                if (deploymentVersionDto != null) {
+                    it.version == deploymentVersionDto
+                } else {
+                    true
+                }
+            }
+        if (lastStatuses.isNullOrEmpty()) {
+            throw NoEventReceiverStatusFound()
+        }
+        val receiverStatuses =
+            lastStatuses.map { receiverStatuses ->
+                EventReceiverStatusDto(
+                    receiverStatuses =
+                        receiverStatuses.receiverStatuses.map { receiverStatus ->
+                            ReceiverStatusDto(
+                                status =
+                                    receiverStatus.status.let {
+                                        ReceiverStatusDto.Status.valueOf(it.toString())
+                                    },
+                                name = receiverStatus.name
+                            )
+                        },
+                    instanceId = receiverStatuses.consumerInstanceId,
+                    deploymentVersion = receiverStatuses.version
+                )
+            }
+
+        return EventReceiverStatusResponseDto(status = receiverStatuses)
+    }
+}
---
+++ src/test/kotlin/it/pagopa/ecommerce/transactions/scheduler/services/EventReceiverServiceTest.kt
@@ -0,0 +1,218 @@
+package it.pagopa.ecommerce.transactions.scheduler.services
+
+import it.pagopa.ecommerce.transactions.scheduler.configurations.RedisStreamEventControllerConfigs
+import it.pagopa.ecommerce.transactions.scheduler.configurations.redis.EventDispatcherCommandsTemplateWrapper
+import it.pagopa.ecommerce.transactions.scheduler.configurations.redis.EventDispatcherReceiverStatusTemplateWrapper
+import it.pagopa.ecommerce.transactions.scheduler.exceptions.NoEventReceiverStatusFound
+import it.pagopa.ecommerce.transactions.scheduler.repositories.redis.eventreceivers.ReceiverStatus
+import it.pagopa.ecommerce.transactions.scheduler.repositories.redis.eventreceivers.ReceiversStatus
+import it.pagopa.ecommerce.transactions.scheduler.repositories.redis.eventreceivers.Status
+import it.pagopa.ecommerce.transactions.scheduler.streams.commands.EventDispatcherReceiverCommand
+import it.pagopa.generated.scheduler.server.model.*
+import java.time.OffsetDateTime
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+import org.mockito.kotlin.*
+import org.springframework.data.redis.connection.stream.RecordId
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class EventReceiverServiceTest {
+
+    private val eventDispatcherCommandsTemplateWrapper: EventDispatcherCommandsTemplateWrapper =
+        mock()
+    private val eventDispatcherReceiverStatusTemplateWrapper:
+        EventDispatcherReceiverStatusTemplateWrapper =
+        mock()
+    private val redisStreamConf =
+        RedisStreamEventControllerConfigs(
+            streamKey = "streamKey",
+            consumerNamePrefix = "consumerNamePrefix"
+        )
+
+    private val eventReceiverService =
+        EventReceiverService(
+            eventDispatcherReceiverStatusTemplateWrapper =
+                eventDispatcherReceiverStatusTemplateWrapper,
+            redisStreamConf = redisStreamConf,
+            eventDispatcherCommandsTemplateWrapper = eventDispatcherCommandsTemplateWrapper
+        )
+
+    @ParameterizedTest
+    @EnumSource(EventReceiverCommandRequestDto.Command::class)
+    fun `Should handle command correctly`(requestCommand: EventReceiverCommandRequestDto.Command) =
+        runTest {
+            // pre-requisites
+            val deploymentVersion = DeploymentVersionDto.PROD
+            val request =
+                EventReceiverCommandRequestDto(
+                    command = requestCommand,
+                    deploymentVersion = deploymentVersion
+                )
+            val expectedCommand =
+                EventDispatcherReceiverCommand(
+                    receiverCommand =
+                        EventDispatcherReceiverCommand.ReceiverCommand.valueOf(
+                            requestCommand.toString()
+                        ),
+                    version = deploymentVersion
+                )
+            given(
+                    eventDispatcherCommandsTemplateWrapper.writeEventToStreamTrimmingEvents(
+                        any(),
+                        any(),
+                        any()
+                    )
+                )
+                .willReturn(RecordId.autoGenerate())
+            // test
+            eventReceiverService.handleCommand(request)
+            // assertions
+            verify(eventDispatcherCommandsTemplateWrapper, times(1))
+                .writeEventToStreamTrimmingEvents(redisStreamConf.streamKey, expectedCommand, 0)
+        }
+
+    @Test
+    fun `Should retrieve receiver statuses successfully for all deployment versions`() = runTest {
+        // pre-requisites
+        val receiverStatuses =
+            listOf(
+                ReceiversStatus(
+                    receiverStatuses =
+                        listOf(ReceiverStatus(name = "receiverName1", status = Status.DOWN)),
+                    version = DeploymentVersionDto.PROD,
+                    queriedAt = OffsetDateTime.now().toString(),
+                    consumerInstanceId = "consumerInstanceId1"
+                ),
+                ReceiversStatus(
+                    receiverStatuses =
+                        listOf(ReceiverStatus(name = "receiverName2", status = Status.UP)),
+                    version = DeploymentVersionDto.STAGING,
+                    queriedAt = OffsetDateTime.now().toString(),
+                    consumerInstanceId = "consumerInstanceId2"
+                )
+            )
+        val expectedResponse =
+            EventReceiverStatusResponseDto(
+                status =
+                    listOf(
+                        EventReceiverStatusDto(
+                            instanceId = "consumerInstanceId1",
+                            receiverStatuses =
+                                listOf(
+                                    ReceiverStatusDto(
+                                        name = "receiverName1",
+                                        status = ReceiverStatusDto.Status.DOWN
+                                    )
+                                ),
+                            deploymentVersion = DeploymentVersionDto.PROD
+                        ),
+                        EventReceiverStatusDto(
+                            instanceId = "consumerInstanceId2",
+                            receiverStatuses =
+                                listOf(
+                                    ReceiverStatusDto(
+                                        name = "receiverName2",
+                                        status = ReceiverStatusDto.Status.UP
+                                    )
+                                ),
+                            deploymentVersion = DeploymentVersionDto.STAGING
+                        )
+                    )
+            )
+        given(eventDispatcherReceiverStatusTemplateWrapper.allValuesInKeySpace)
+            .willReturn(receiverStatuses.toMutableList())
+
+        // test
+        val response = eventReceiverService.getReceiversStatus(deploymentVersionDto = null)
+        // assertions
+        assertEquals(expectedResponse, response)
+    }
+
+    @Test
+    fun `Should retrieve receiver statuses successfully filtering for deployment version`() =
+        runTest {
+            // pre-requisites
+            val receiverStatuses =
+                listOf(
+                    ReceiversStatus(
+                        receiverStatuses =
+                            listOf(ReceiverStatus(name = "receiverName1", status = Status.DOWN)),
+                        version = DeploymentVersionDto.PROD,
+                        queriedAt = OffsetDateTime.now().toString(),
+                        consumerInstanceId = "consumerInstanceId1"
+                    ),
+                    ReceiversStatus(
+                        receiverStatuses =
+                            listOf(ReceiverStatus(name = "receiverName2", status = Status.UP)),
+                        version = DeploymentVersionDto.STAGING,
+                        queriedAt = OffsetDateTime.now().toString(),
+                        consumerInstanceId = "consumerInstanceId2"
+                    )
+                )
+            val expectedResponse =
+                EventReceiverStatusResponseDto(
+                    status =
+                        listOf(
+                            EventReceiverStatusDto(
+                                instanceId = "consumerInstanceId1",
+                                receiverStatuses =
+                                    listOf(
+                                        ReceiverStatusDto(
+                                            name = "receiverName1",
+                                            status = ReceiverStatusDto.Status.DOWN
+                                        )
+                                    ),
+                                deploymentVersion = DeploymentVersionDto.PROD
+                            )
+                        )
+                )
+            given(eventDispatcherReceiverStatusTemplateWrapper.allValuesInKeySpace)
+                .willReturn(receiverStatuses.toMutableList())
+
+            // test
+            val response =
+                eventReceiverService.getReceiversStatus(
+                    deploymentVersionDto = DeploymentVersionDto.PROD
+                )
+            // assertions
+            assertEquals(expectedResponse, response)
+        }
+
+    @Test
+    fun `Should throw NoEventReceiverStatusFound for no data found retrieving receiver statuses`() =
+        runTest {
+            // pre-requisites
+            val receiverStatuses =
+                listOf(
+                    ReceiversStatus(
+                        receiverStatuses =
+                            listOf(ReceiverStatus(name = "receiverName1", status = Status.DOWN)),
+                        version = DeploymentVersionDto.STAGING,
+                        queriedAt = OffsetDateTime.now().toString(),
+                        consumerInstanceId = "consumerInstanceId1"
+                    ),
+                    ReceiversStatus(
+                        receiverStatuses =
+                            listOf(ReceiverStatus(name = "receiverName2", status = Status.UP)),
+                        version = DeploymentVersionDto.STAGING,
+                        queriedAt = OffsetDateTime.now().toString(),
+                        consumerInstanceId = "consumerInstanceId2"
+                    )
+                )
+
+            given(eventDispatcherReceiverStatusTemplateWrapper.allValuesInKeySpace)
+                .willReturn(receiverStatuses.toMutableList())
+
+            // test
+            assertThrows<NoEventReceiverStatusFound> {
+                eventReceiverService.getReceiversStatus(
+                    deploymentVersionDto = DeploymentVersionDto.PROD
+                )
+            }
+        }
+}
