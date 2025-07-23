2e2129a71c718b16ff338b5859567bc341b5fb99
sfarralorenzo <lorenzo.sfarra@pagopa.it>
2025-07-23 11:55:56 +0200

    feat: configuration to control the transaction view collection updates

---
+++ src/main/kotlin/it/pagopa/ecommerce/transactions/scheduler/configurations/TransactionViewConfig.kt
@@ -0,0 +1,25 @@
+package it.pagopa.ecommerce.transactions.scheduler.configurations
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Configuration for transaction view collection management.
+ * Controls whether the service updates the MongoDB transaction-view collection.
+ */
+@Configuration
+class TransactionViewConfig {
+
+    /**
+     * Configuration bean for transaction view update feature flag
+     *
+     * @param transactionViewUpdateEnabled
+     * - boolean flag to enable/disable transaction view updates
+     * @return the configuration value
+     */
+    @Bean
+    fun transactionViewUpdateEnabled(
+        @Value("\${transactionview.update.enabled}") transactionViewUpdateEnabled: Boolean
+    ): Boolean = transactionViewUpdateEnabled
+}
