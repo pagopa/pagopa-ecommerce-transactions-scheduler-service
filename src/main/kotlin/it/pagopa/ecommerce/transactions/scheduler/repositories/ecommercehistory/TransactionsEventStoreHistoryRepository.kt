4b58843636b860d938bb1cc719dd93751c328d0e
sfarralorenzo <lorenzo.sfarra@pagopa.it>
2025-10-28 15:47:33 +0100

    chore: remove unused repository methods

--- src/main/kotlin/it/pagopa/ecommerce/transactions/scheduler/repositories/ecommercehistory/TransactionsEventStoreHistoryRepository.kt
+++ src/main/kotlin/it/pagopa/ecommerce/transactions/scheduler/repositories/ecommercehistory/TransactionsEventStoreHistoryRepository.kt
@@ -3,12 +3,7 @@ package it.pagopa.ecommerce.transactions.scheduler.repositories.ecommercehistory
 import it.pagopa.ecommerce.commons.documents.BaseTransactionEvent
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
-import reactor.core.publisher.Flux
 
 @Repository
 interface TransactionsEventStoreHistoryRepository<T> :
-    ReactiveCrudRepository<BaseTransactionEvent<T>, String> {
-    fun findByTransactionIdOrderByCreationDateAsc(
-        idTransaction: String
-    ): Flux<BaseTransactionEvent<T>>
-}
+    ReactiveCrudRepository<BaseTransactionEvent<T>, String> {}
--- src/main/kotlin/it/pagopa/ecommerce/transactions/scheduler/repositories/ecommercehistory/TransactionsViewHistoryRepository.kt
+++ src/main/kotlin/it/pagopa/ecommerce/transactions/scheduler/repositories/ecommercehistory/TransactionsViewHistoryRepository.kt
@@ -3,10 +3,6 @@ package it.pagopa.ecommerce.transactions.scheduler.repositories.ecommercehistory
 import it.pagopa.ecommerce.commons.documents.BaseTransactionView
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
-import reactor.core.publisher.Mono
 
 @Repository
-interface TransactionsViewHistoryRepository : ReactiveCrudRepository<BaseTransactionView, String> {
-
-    fun findByTransactionId(transactionId: String): Mono<BaseTransactionView>
-}
+interface TransactionsViewHistoryRepository : ReactiveCrudRepository<BaseTransactionView, String> {}
