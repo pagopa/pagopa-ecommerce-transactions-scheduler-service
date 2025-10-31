83fde05f450ecdfa508b8ed583dcab5371bf0cac
danilo.ciano <danilo.ciano@pagopa.it>
2025-10-31 14:29:23 +0100

    feat(migration): add TransactionsViewMigrationBatch test

---
+++ src/test/kotlin/it/pagopa/ecommerce/transactions/scheduler/scheduledperations/TransactionsViewMigrationBatchTest.kt
@@ -0,0 +1,27 @@
+package it.pagopa.ecommerce.transactions.scheduler.scheduledperations
+
+import it.pagopa.ecommerce.transactions.scheduler.services.TransactionsViewMigrationOrchestrator
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+
+@ExtendWith(MockitoExtension::class)
+class TransactionsViewMigrationBatchTest {
+    @Mock
+    private lateinit var transactionsViewMigrationOrchestrator: TransactionsViewMigrationOrchestrator
+    @InjectMocks
+    private lateinit var transactionsViewMigrationBatch: TransactionsViewMigrationBatch
+
+    @Test
+    fun `execute should call runMigration on orchestrator`() {
+        // Act
+        transactionsViewMigrationBatch.execute()
+
+        // Assert
+        verify(transactionsViewMigrationOrchestrator, times(1)).runMigration()
+    }
+}
