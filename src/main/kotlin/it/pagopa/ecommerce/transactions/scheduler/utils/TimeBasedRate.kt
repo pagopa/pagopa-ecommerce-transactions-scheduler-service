9b04337289459439a66e9ff12b2791121b3e755d
pietro.tota <pietro.tota@pagopa.it>
2026-03-27 18:16:17 +0100

    chore: add kdoc

--- src/main/kotlin/it/pagopa/ecommerce/transactions/scheduler/utils/TimeBasedRate.kt
+++ src/main/kotlin/it/pagopa/ecommerce/transactions/scheduler/utils/TimeBasedRate.kt
@@ -6,7 +6,22 @@ import java.time.LocalTime
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-/** This class models a time based rate */
+/**
+ * This class models a dynamic time based rate calculator utility. The rate will be automatically
+ * calculated based on input time window configuration and current time. This class is configured by
+ * the following parameters:
+ * - [lowRate] & [highRate] respectively the min and max rate that will be returned by the
+ * [calculateRate] method. both must be positive numbers with highRate >= lowRate
+ * - [from] & [to] define the time window where the [highRate] or linear increase from the [lowRate]
+ * will be returned.
+ * - [rampUp] the ramping up duration. Ramping up period cannot be longer than the time window
+ * defined by [from] and [to] parameters
+ *
+ * The rate is calculated as lowRate outside the defined window, highRate inside the time window
+ * with linear ramp up strategy starting from the window [from] for the configured [rampUp] duration
+ *
+ * see [calculateRate] method for more details on rate calculation
+ */
 class TimeBasedRate(
     val from: LocalTime,
     val to: LocalTime,
@@ -16,6 +31,7 @@ class TimeBasedRate(
 ) {
 
     companion object {
+        /** Construct [TimeBasedRate] class reading parameters from [QuerySettings] object */
         fun fromQuerySettings(querySettings: QuerySettings): TimeBasedRate =
             TimeBasedRate(
                 from = querySettings.burstStartWindow,
@@ -44,6 +60,23 @@ class TimeBasedRate(
         }
     }
 
+    /**
+     * This is the method that calculate the rate based on the [at] parameter (defaulted to now) and
+     * the [TimeBasedRate] configuration. The rate will be calculated as [lowRate] outside of the
+     * time window, [highRate] inside time window with linear ramping up strategy
+     *
+     * Supposing to have the following configuration:
+     * - from: 22:00, to: 02:00
+     * - lowRate: 100, highRate: 200
+     * - rampUp: 2h
+     *
+     * with this configuration this method will return the following values for the [at] parameter:
+     * - at in [[02:00 - 22:00]] -> 100
+     * - at = 23:00 -> 150
+     * - at = 00:00 -> 200
+     * - at in [[00:00 - 02:00]] -> 200
+     * - and here the cycle repeat
+     */
     fun calculateRate(at: LocalTime = LocalTime.now()): Int {
         // case where temporal window is in the same day. ex. 09:00 (d) -> 18:00 (d)
         val (rangeElapsedTime, isInRange) =
