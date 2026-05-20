86c55a61df279645aeb28792fb302f31e1f7de62
Stefano Rondinella <stefrondinella@live.it>
2026-05-20 17:57:20 +0200

    feat(integration-test): add js script for the integration test

---
+++ integration-test/migration-integration-test.js
@@ -0,0 +1,143 @@
+console.log("Starting integration test...")
+
+// The service will find only wallet older than 9 months to move in the history db
+let notValidDateString = new Date().toISOString();
+
+// Add testing event to the db
+let eventList = [
+    {
+      "_id": "a5ded083-5d8d-4771-8215-d01c1346fzz1"
+      "transactionId": "075f6031-005c-4927-9a3d-e613d60cafba",
+      "creationDate": "2023-03-29T16:44:14.959647218Z[Etc/UTC]",
+      "data": {
+        "email": {
+          "data": "b282accf-995f-4211-939c-1ba0b4f3f255"
+        },
+        "paymentNotices": [
+          {
+            "paymentToken": "3d82a804063f46ed99dfa9e4a235774d",
+            "rptId": "77777777777302001241098804227",
+            "description": "TARI/TEFA 2021",
+            "amount": 1000,
+            "isAllCCP": false
+          }
+        ],
+        "clientId": "CHECKOUT",
+        "paymentTokenValiditySeconds": 0
+      },
+      "eventCode": "TRANSACTION_ACTIVATED_EVENT",
+      "_class": "it.pagopa.ecommerce.commons.documents.v1.TransactionActivatedEvent"
+    },
+    {
+      "_id": "a5ded083-5d8d-4771-8215-d01c1346fzz2"
+      "transactionId": "522ab4f9-8d96-4bc7-a028-f0699d1baea2",
+      "creationDate": "2023-03-29T16:44:15.020618605Z[Etc/UTC]",
+      "data": {
+        "email": {
+          "data": "b282accf-995f-4211-939c-1ba0b4f3f255"
+        },
+        "paymentNotices": [
+          {
+            "paymentToken": "3d82a804063f46ed99dfa9e4a235774d",
+            "rptId": "77777777777302001210146638735",
+            "description": "TARI/TEFA 2021",
+            "amount": 1000,
+            "isAllCCP": false
+          }
+        ],
+        "clientId": "CHECKOUT",
+        "paymentTokenValiditySeconds": 0
+      },
+      "eventCode": "TRANSACTION_ACTIVATED_EVENT",
+      "_class": "it.pagopa.ecommerce.commons.documents.v1.TransactionActivatedEvent"
+    },
+    {
+      "_id": "a5ded083-5d8d-4771-8215-d01c1346fzz3"
+      "transactionId": "da487724-1078-4f20-b095-ff45793e758b",
+      "creationDate": "2023-03-29T16:44:15.119048976Z[Etc/UTC]",
+      "data": {
+        "email": {
+          "data": "b282accf-995f-4211-939c-1ba0b4f3f255"
+        },
+        "paymentNotices": [
+          {
+            "paymentToken": "3d82a804063f46ed99dfa9e4a235774d",
+            "rptId": "77777777777302001250013262570",
+            "description": "TARI/TEFA 2021",
+            "amount": 1000,
+            "isAllCCP": false
+          }
+        ],
+        "clientId": "CHECKOUT",
+        "paymentTokenValiditySeconds": 0
+      },
+      "eventCode": "TRANSACTION_ACTIVATED_EVENT",
+      "_class": "it.pagopa.ecommerce.commons.documents.v1.TransactionActivatedEvent"
+    }
+    // Should not be migrate: too recent
+    {
+     "_id": "a5ded083-5d8d-4771-8215-d01c1346fzz4"
+     "transactionId": "zz587724-1078-4f20-b095-ff45793e758b",
+     "creationDate": notValidDateString,
+     "data": {
+       "email": {
+         "data": "b282accf-995f-4211-939c-1ba0b4f3f255"
+       },
+       "paymentNotices": [
+         {
+           "paymentToken": "3d82a804063f46ed99dfa9e4a235774d",
+           "rptId": "77777777777302001250013262570",
+           "description": "TARI/TEFA 2021",
+           "amount": 1000,
+           "isAllCCP": false
+         }
+       ],
+       "clientId": "CHECKOUT",
+       "paymentTokenValiditySeconds": 0
+     },
+     "eventCode": "TRANSACTION_ACTIVATED_EVENT",
+     "_class": "it.pagopa.ecommerce.commons.documents.v1.TransactionActivatedEvent"
+   }
+]
+
+
+//connect to Mongo DB
+conn = new Mongo();
+dbHistory = conn.getDB("ecommerce-history");
+//create and populate ecommerce and ecommerce-history collections
+dbHistory.getCollection('eventstore').insertMany([eventList[0],eventList[1]])
+
+db = conn.getDB("ecommerce")
+db.getCollection('eventstore').insertMany([eventList[1],eventList[2],eventList[3]])
+
+// Wait until the scheduler do the job and then check the result
+await setTimeout(20000);
+
+// Check that the inserted data are moved from ecommerce.eventstore to ecommerce-history.eventstore
+function assertMigration(eventList, dbCollection, dbHistoryCollection){
+    let docsDbArray = dbCollection.getCollection('eventstore').find().toArray()
+    let docsDbHistoryArray = dbHistoryCollection.getCollection('eventstore').find().toArray()
+
+    // Check the transaction that were not in the ecommerce-history now are migrated by the script
+    let documentAreMigrated = eventList.slice(0,eventList.length-1).reduce(
+        (accumulator, currentValue) => accumulator && docsDbHistoryArray.contains(currentValue),
+        true,
+    );
+
+    // Check that the recent event was not migrated
+    let recentEvent = eventList[eventList.length-1]
+    let tooRecentDocumentNotMigrated = !docsDbHistoryArray.contains(currentValue)
+
+    console.log("Test assert: ",documentAreMigrated && tooRecentDocumentNotMigrated)
+
+    return documentAreMigrated && tooRecentDocumentNotMigrated;
+}
+
+if(!assertMigration(eventList, db, dbHistory)){
+    console.log("Integration test failed!");
+    quit(1);
+}else{
+    console.log("Integration test completed SUCCESSFULLY!");
+}
+
+
