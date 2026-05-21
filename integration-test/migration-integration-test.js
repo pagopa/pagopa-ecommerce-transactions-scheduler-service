804c7b7bcdca20b929ce5761062de329a1a775e7
Stefano Rondinella <stefrondinella@live.it>
2026-05-21 14:59:21 +0200

    fix(integration-test): fix

--- integration-test/migration-integration-test.js
+++ integration-test/migration-integration-test.js
@@ -27,7 +27,7 @@ let notValidDateString = new Date().toISOString();
 // Add testing event to the db
 let eventList = [
     {
-      "_id": "a5ded083-5d8d-4771-8215-d01c1346fzz1"
+      "_id": "a5ded083-5d8d-4771-8215-d01c1346fzz1",
       "transactionId": "075f6031-005c-4927-9a3d-e613d60cafba",
       "creationDate": "2023-03-29T16:44:14.959647218Z[Etc/UTC]",
       "data": {
@@ -50,7 +50,7 @@ let eventList = [
       "_class": "it.pagopa.ecommerce.commons.documents.v1.TransactionActivatedEvent"
     },
     {
-      "_id": "a5ded083-5d8d-4771-8215-d01c1346fzz2"
+      "_id": "a5ded083-5d8d-4771-8215-d01c1346fzz2",
       "transactionId": "522ab4f9-8d96-4bc7-a028-f0699d1baea2",
       "creationDate": "2023-03-29T16:44:15.020618605Z[Etc/UTC]",
       "data": {
@@ -73,7 +73,7 @@ let eventList = [
       "_class": "it.pagopa.ecommerce.commons.documents.v1.TransactionActivatedEvent"
     },
     {
-      "_id": "a5ded083-5d8d-4771-8215-d01c1346fzz3"
+      "_id": "a5ded083-5d8d-4771-8215-d01c1346fzz3",
       "transactionId": "da487724-1078-4f20-b095-ff45793e758b",
       "creationDate": "2023-03-29T16:44:15.119048976Z[Etc/UTC]",
       "data": {
@@ -94,10 +94,10 @@ let eventList = [
       },
       "eventCode": "TRANSACTION_ACTIVATED_EVENT",
       "_class": "it.pagopa.ecommerce.commons.documents.v1.TransactionActivatedEvent"
-    }
+    },
     // Should not be migrate: too recent
     {
-     "_id": "a5ded083-5d8d-4771-8215-d01c1346fzz4"
+     "_id": "a5ded083-5d8d-4771-8215-d01c1346fzz4",
      "transactionId": "zz587724-1078-4f20-b095-ff45793e758b",
      "creationDate": notValidDateString,
      "data": {
@@ -122,43 +122,45 @@ let eventList = [
 ]
 
 
-//connect to Mongo DB
-conn = new Mongo();
-dbHistory = conn.getDB("ecommerce-history");
-//create and populate ecommerce and ecommerce-history collections
-dbHistory.getCollection('eventstore').insertMany([eventList[0],eventList[1]])
 
-db = conn.getDB("ecommerce")
+console.log("Insert testing data into ecommerce.eventstore and ecommerce-history.eventstore");
+//connect to Mongo DB
+dbHistory = db.getSiblingDB("ecommerce-history");
+dbHistory.getCollection('eventstore').insertMany([eventList[0],eventList[1]])
+db = db.getSiblingDB("ecommerce")
 db.getCollection('eventstore').insertMany([eventList[1],eventList[2],eventList[3]])
 
 // Wait until the scheduler do the job and then check the result
-await setTimeout(20000);
-
-// Check that the inserted data are moved from ecommerce.eventstore to ecommerce-history.eventstore
-function assertMigration(eventList, dbCollection, dbHistoryCollection){
-    let docsIdDbArray = dbCollection.getCollection('eventstore').find().toArray().map(e -> e._id);
-    let docsIdDbHistoryArray = dbHistoryCollection.getCollection('eventstore').find().toArray().map(e -> e._id);
-
-    // Check the transaction that were not in the ecommerce-history now are migrated by the script
-    let documentAreMigrated = eventList.slice(0,eventList.length-1).map(e -> e._id).reduce(
-        (accumulator, currentIdValue) => accumulator && docsIdDbHistoryArray.contains(currentIdValue),
-        true,
-    );
-
-    // Check that the recent event was not migrated
-    let recentEvent = eventList[eventList.length-1]
-    let tooRecentDocumentNotMigrated = !docsIdDbHistoryArray.contains(recentEvent._id)
-
-    console.log("Test assert: ",documentAreMigrated && tooRecentDocumentNotMigrated)
-
-    return documentAreMigrated && tooRecentDocumentNotMigrated;
-}
+console.log("Wait until the migration job is executed...")
+sleep(20000);
 
 if(!assertMigration(eventList, db, dbHistory)){
     console.log("Integration test failed!");
     quit(1);
 }else{
     console.log("Integration test completed SUCCESSFULLY!");
+}
+
+
+
+// Check that the inserted data are moved from ecommerce.eventstore to ecommerce-history.eventstore
+function assertMigration(eventList, dbCollection, dbHistoryCollection){
+    let docsIdDbArray = dbCollection.getCollection('eventstore').find().toArray().map(e => e._id);
+    let docsIdDbHistoryArray = dbHistoryCollection.getCollection('eventstore').find().toArray().map(e => e._id);
+
+    // Check the transaction that were not in the ecommerce-history now are migrated by the script
+    let documentAreMigrated = eventList.slice(0,eventList.length-1).map(e => e._id).reduce(
+        (accumulator, currentIdValue) => accumulator && docsIdDbHistoryArray.includes(currentIdValue),
+        true,
+    );
+
+    // Check that the recent event was not migrated
+    let recentEvent = eventList[eventList.length-1]
+    let tooRecentDocumentNotMigrated = !docsIdDbHistoryArray.includes(recentEvent._id)
+
+    console.log("Test assert: ",documentAreMigrated && tooRecentDocumentNotMigrated)
+
+    return documentAreMigrated && tooRecentDocumentNotMigrated;
 }
 
 
